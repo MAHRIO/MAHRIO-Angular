a709ec06fe6a9e80bbbda18d60b8912d1f4f2c57
emcp <emcp@whichdegree.co>
2019-08-10 17:33:48 +0200

    Working product, angular app hosted on hapijs

--- server/routes/static/static.routing.js
+++ server/routes/static/static.routing.js
@@ -13,19 +13,22 @@ module.exports = function(server, root, publicPath) {
   server.route({
     path: '/{any}',
     method: 'GET',
-    handler: function(req, rep) {
+    handler: function(req, h) {
+      console.log("static.routing.GET: ")
       if( fs.existsSync(root+'/public/'+publicPath + '/' + process.env.NODE_ENV + '/'+req.params.any) ) {
-        rep.file(publicPath + '/' + process.env.NODE_ENV + '/'+req.params.any);
+        return h.file(publicPath + '/' + process.env.NODE_ENV + '/'+req.params.any);
       } else {
-        rep.file(publicPath + '/' + process.env.NODE_ENV + '/index.html');
+        console.log("static.routing.GET: requested " + publicPath + '/' + process.env.NODE_ENV + '/index.html')
+        return h.file(publicPath + '/' + process.env.NODE_ENV + '/index.html');
       }
     }
   });
   server.route({
     path: '/{any*}',
     method: 'GET',
-    handler: function(req, rep) {
-      rep.file(publicPath + '/' + process.env.NODE_ENV + '/index.html');
+    handler: function(req, h) {
+      console.log("static.routing.GET requesting " +publicPath + '/' + process.env.NODE_ENV + '/index.html')
+      return h.file(publicPath + '/' + process.env.NODE_ENV + '/index.html');
     }
   });
 };
